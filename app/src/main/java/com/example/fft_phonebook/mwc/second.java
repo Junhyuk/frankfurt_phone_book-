a8831e2a742b0556fe5845860df8b18ccf4b2fe7
Junhyuk Lee <junhyuk7.lee@gmail.com>
2016-06-26 23:56:42 +0900

    second commit for ui

--- app/src/main/java/com/example/fft_phonebook/mwc/second.java
+++ app/src/main/java/com/example/fft_phonebook/mwc/second.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.RelativeSizeSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.text.SpannableStringBuilder;
 
 import static com.example.fft_phonebook.mwc.R.id.button;
 
@@ -25,6 +29,7 @@ public class second extends Activity  implements View.OnClickListener {
 
     String g_addr;
     String g_phone_num;
+    String g_menu;
     private View arg0;
     private GoogleMap map;
 
@@ -43,9 +48,11 @@ public class second extends Activity  implements View.OnClickListener {
         String text = bundle.getString("arr_text");
         String addr_text = bundle.getString("addr_text");
         String phone_text= bundle.getString("phone_num");
+        String menu = bundle.getString("main_menu");
 
         g_addr = addr_text;
         g_phone_num = phone_text;
+        g_menu = menu;
 
 
         TextView sc_t = (TextView) findViewById(R.id.LargeText);
@@ -54,13 +61,27 @@ public class second extends Activity  implements View.OnClickListener {
 
 
         TextView sc_addr = (TextView) findViewById(R.id.textView);
-        sc_addr.setText(addr_text);
+
+        addr_text = "Address: " +"\n"+ addr_text;
+
+        final SpannableStringBuilder sps = new SpannableStringBuilder(addr_text);
+        sps.setSpan(new RelativeSizeSpan(1.2f), 0, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sc_addr.setText(sps, TextView.BufferType.SPANNABLE);
+
+        //sc_addr.append(sps);
+        //sc_addr.setText(addr_text);
 
         TextView sc_phone = (TextView) findViewById(R.id.textView2);
 
         SpannableString content = new SpannableString(phone_text);
         content.setSpan(new UnderlineSpan(), 0, phone_text.length(), 0);
         sc_phone.setText(content);
+
+
+
+        TextView sc_menu = (TextView) findViewById(R.id.Menu);
+        sc_menu.setText(menu);
+
 
 
         Log.i("MWC", "second second" + addr_text);
